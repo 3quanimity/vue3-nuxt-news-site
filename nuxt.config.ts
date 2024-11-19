076fe48824587e96539b87163dbfc8948828fd13
Haythem <sinnner6@gmail.com>
2024-11-19 17:23:44 +0100

    display news from an api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  css: ['~/node_modules/bootstrap/dist/css/bootstrap.min.css']
+  css: ['~/node_modules/bootstrap/dist/css/bootstrap.min.css'],
+
+  runtimeConfig: {
+    // https://www.thenewsapi.com/documentation
+    // TODO: should use apiSecret instead of hardcoding the token into the apiBase
+    apiSecret: 'FtLRaK0ZATRDL2rtWjWDYriArlkAG8DoMVO7oPJy',
+
+    public: {
+      apiBase: 'https://api.thenewsapi.com/v1/news/top?locale=us&limit=3&api_token=FtLRaK0ZATRDL2rtWjWDYriArlkAG8DoMVO7oPJy'
+    }
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,19 @@
 <template>
     <h1>Actus</h1>
+    <div class="row align-items-start">
+        <div v-for="actu in data.data" class="card col" style="width: 18rem;">
+            <img :src="actu.image_url" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">{{ actu.title.slice(0, 100) }}</h5>
+                <p class="card-text">{{ actu.description.slice(0, 300) }}</p>
+                <a :href="actu.url" class="btn btn-primary">Lire la suite</a>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script lang="ts" setup>
+    const runtimeConfig = useRuntimeConfig()
+
+    const { data } = await useFetch(runtimeConfig.public.apiBase)
+</script>
